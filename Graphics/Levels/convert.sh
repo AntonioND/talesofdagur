6cac2fceb9cf7d59b88c4b36dd0055a43c304967
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-07-15 19:04:04 +0100

    Add script to convert graphics in Levels directory
    Levels 6, 13 and 16 have too many colors and the output is bad, they
    need to be fixed.

---
+++ Graphics/Levels/convert.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -x
+
+GRIT=/opt/blocksds/core/tools/grit/grit
+
+GRAPHICS=../../arm9/graphics/
+OUT=${GRAPHICS}/Levels/
+
+rm -rf ${OUT}
+mkdir -p ${OUT}
+
+do_map() {
+    NUM=$1
+
+    $GRIT level${NUM}Ground.bmp level${NUM}Down.bmp level${NUM}Up.bmp -ftc -fh -W2 \
+        -gB8 -gt -gTFF00FF -gzl -pS -m -mR8 \
+        -D${OUT} -O${OUT}level${NUM}Pal
+
+    $GRIT level${NUM}Col.bmp -ftc -fh -W2 \
+        -gB8 -gt -gT000000 -g! -m -mLa -p! \
+        -o${OUT}level${NUM}Col
+}
+
+do_map 1
+do_map 2
+do_map 3
+do_map 4
+do_map 5
+do_map 6
+do_map 7
+do_map 8
+do_map 9
+do_map 10
+do_map 11
+do_map 12
+do_map 13
+do_map 14
+do_map 15
+do_map 16
+do_map 17
+do_map 18
